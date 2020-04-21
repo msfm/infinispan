9dc2cf5947a3860d8998aa6c968c04bd1b791ba9
Masafumi Miura <mmiura@redhat.com>
2020-04-21 16:26:05 +0900

    ISPN-11679 Add testRebalanceDistribution() in TopologyAwareSyncConsistentHashFactoryKeyDistributionTest

--- core/src/test/java/org/infinispan/distribution/ch/TopologyAwareSyncConsistentHashFactoryKeyDistributionTest.java
+++ core/src/test/java/org/infinispan/distribution/ch/TopologyAwareSyncConsistentHashFactoryKeyDistributionTest.java
@@ -1,10 +1,17 @@
 package org.infinispan.distribution.ch;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.infinispan.commons.hash.MurmurHash3;
 import org.infinispan.distribution.ch.impl.DefaultConsistentHash;
+import org.infinispan.distribution.ch.impl.OwnershipStatistics;
 import org.infinispan.distribution.ch.impl.TopologyAwareSyncConsistentHashFactory;
 import org.infinispan.remoting.transport.Address;
 import org.infinispan.remoting.transport.jgroups.JGroupsTopologyAwareAddress;
@@ -45,12 +52,90 @@ public class TopologyAwareSyncConsistentHashFactoryKeyDistributionTest extends S
    protected List<Address> createAddresses(int numNodes) {
       ArrayList<Address> addresses = new ArrayList<Address>(numNodes);
       for (int i = 0; i < numNodes; i++) {
-         ExtendedUUID topologyAddress = JGroupsTopologyAwareAddress.randomUUID(null, "s" + (i % 2), null, "m" + i);
+         // siteId: s0 or s1, rackId: null, machineId: m0 - mXX (each nodes)
+         // ExtendedUUID topologyAddress = JGroupsTopologyAwareAddress.randomUUID(null, "s" + (i % 2), null, "m" + i);
+         // siteId: s1, rackId: r0 or r1, machineId: m0 - mXX (each nodes)
+         ExtendedUUID topologyAddress = JGroupsTopologyAwareAddress.randomUUID(null, "s1", "r" + (i % 2), "m" + i);
          addresses.add(new IndexedTopologyAwareJGroupsAddress(topologyAddress, i));
       }
       return addresses;
    }
 
+   @Override
+   public void testRebalanceDistribution() {
+      for (int nn : NUM_NODES) {
+         Map<String, Map<Integer, String>> metrics = new TreeMap<String, Map<Integer, String>>();
+         for (int ns : NUM_SEGMENTS) {
+            for (Map.Entry<String, String> entry : computeMetricsAfterRebalance(ns, NUM_OWNERS, nn).entrySet()) {
+               String metricName = entry.getKey();
+               String metricValue = entry.getValue();
+               Map<Integer, String> metric = metrics.get(metricName);
+               if (metric == null) {
+                  metric = new HashMap<Integer, String>();
+                  metrics.put(metricName, metric);
+               }
+               metric.put(ns, metricValue);
+            }
+         }
+
+         printMetrics(nn, metrics);
+      }
+   }
+
+   @Override
+   protected Map<String, String> computeMetricsAfterRebalance(int numSegments, int numOwners, int numNodes) {
+      List<Address> members = createAddresses(numNodes);
+      Map<String, String> metrics = new HashMap<String, String>();
+      long[] distribution = new long[LOOPS * numNodes];
+      long[] distributionPrimary = new long[LOOPS * numNodes];
+      double[] largestRatio = new double[LOOPS];
+      int distIndex = 0;
+
+      MurmurHash3 hash = MurmurHash3.getInstance();
+      ConsistentHashFactory<DefaultConsistentHash> chf = new TopologyAwareSyncConsistentHashFactory();
+      DefaultConsistentHash ch = chf.create(hash, numOwners, numSegments, members, null);
+
+      // loop leave/join and rebalance
+      for (int i = 0; i < LOOPS; i++) {
+         // leave
+         IndexedTopologyAwareJGroupsAddress leaver = (IndexedTopologyAwareJGroupsAddress)members.remove(0);
+         int leaverNodeIndex = leaver.getNodeIndex();
+         DefaultConsistentHash rebalancedCH = chf.updateMembers(ch, members, null);
+         ch = chf.rebalance(rebalancedCH);
+         // join
+         Address joiner = createSingleAddresses(leaverNodeIndex, leaver); // leaver
+         members.add(joiner);
+         rebalancedCH = chf.updateMembers(ch, members, null);
+         ch = chf.rebalance(rebalancedCH);
+         // stats after rebalance
+         OwnershipStatistics stats = new OwnershipStatistics(ch, ch.getMembers());
+         assertEquals(numSegments * numOwners, stats.sumOwned());
+         for (Address node : ch.getMembers()) {
+            distribution[distIndex] = stats.getOwned(node);
+            distributionPrimary[distIndex] = stats.getPrimaryOwned(node);
+            distIndex++;
+         }
+         largestRatio[i] = getSegmentsPerNodesMinMaxRatio(ch);
+      }
+      Arrays.sort(distribution);
+      Arrays.sort(distributionPrimary);
+      Arrays.sort(largestRatio);
+
+      addMetrics(metrics, "Any owner:", numSegments, numOwners, numNodes, distribution, INTERVALS);
+      addMetrics(metrics, "Primary:", numSegments, 1, numNodes, distributionPrimary, INTERVALS_PRIMARY);
+      addDoubleMetric(metrics, "Segments per node - max/min ratio", largestRatio[largestRatio.length -1]);
+      return metrics;
+   }
+
+   @Override
+   protected Address createSingleAddresses(int i) {
+      return new IndexedTopologyAwareJGroupsAddress(ExtendedUUID.randomUUID(), i);
+   }
+
+   protected Address createSingleAddresses(int i, IndexedTopologyAwareJGroupsAddress address) {
+      ExtendedUUID topologyAddress = JGroupsTopologyAwareAddress.randomUUID(null, address.getSiteId(), address.getRackId(), address.getMachineId());
+      return new IndexedTopologyAwareJGroupsAddress(topologyAddress, i);
+   }
 }
 
 /**
@@ -62,5 +147,14 @@ class IndexedTopologyAwareJGroupsAddress extends JGroupsTopologyAwareAddress {
    public IndexedTopologyAwareJGroupsAddress(ExtendedUUID address, int nodeIndex) {
       super(address);
       this.nodeIndex = nodeIndex;
+   }
+
+   public int getNodeIndex() {
+      return nodeIndex;
+   }
+
+   @Override
+   public String toString() {
+      return Integer.toString(nodeIndex);
    }
 }
